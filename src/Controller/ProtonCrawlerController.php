2e94e2cd5dc3c379b19d5f54295e863d5f823fec
zmajnnex <zmajnnex@yahoo.com>
2019-12-07 15:43:16 +0100

    TO DO

--- src/Controller/ProtonCrawlerController.php
+++ src/Controller/ProtonCrawlerController.php
@@ -15,76 +15,82 @@ use Symfony\Component\DomCrawler\Crawler as DomCrawler;
 //use GuzzleHttp\Client; 
 class ProtonCrawlerController extends AbstractController
 {
-    
- 
-       
+
+
+
     /**
      * @Route("/proton/crawler", name="proton_crawler")
      */
     public function index(Request $request)
     {
 
-        $url = $request->request->get('crawler_form')['url'];       
+        $url = $request->request->get('crawler_form')['url'];
         $testData = $this->crawlUrl($url);
         // dump($url);
-       // return new Response('Crawling: '. $url . '...');
+        // return new Response('Crawling: '. $url . '...');
         return $this->render('form/results.html.twig', array(
-            'url' => $testData));
+            'url' => $testData
+        ));
     }
-    
 
 
+  /**
+   * TO DO : Fix json, check if url is already crawled
+   */
 
 
-    public function crawlUrl($url){
+    public function crawlUrl($url)
+    {
         /**
- * crawl web page and extract all <a> tag hrefs
- *
- * @param string $url
- *
- * @return array $results
- */
-   
+         * crawl web page and extract all <a> tag hrefs
+         *
+         * @param string $url
+         *
+         * @return array $results
+         */
+
         $client = new Client();
-        $crawler = $client->request('GET', $url);    
-        $fp = fopen('results'.time().'.json','w'); 
+        $crawler = $client->request('GET', $url);
+        $fp = fopen('results' . time() . '.json', 'w');
         /**
          * Anonymous Simfony function
          * iterate trough $nodes, and write links in json 
          * @return  array $links
          */
-       //$links = new \stdClass();
-       
-       $links = array(
-           'url'=>$url,
-           'author'=>null,
-           'visited'=>null,
-           'text'=>null,
-           'title'=>null,
-       );
-       // Get href from a tag
-       $results = $crawler->filter('a')->each(function ($node,$i) use ($fp,$links) {
-             
-              
-             $links['url']= $node->link()->getUri();
-             $links['author'] = 'Proton';
-                 
-         fwrite($fp, json_encode($links));
-           //return $links['url']; 
+        //$links = new \stdClass();
+
+        $links = array(
+            'url' => $url,
+            'author' => null,
+            'visited' => null,
+            'text' => null,
+            'title' => null,
+        );
+        // Get href from a tag
+        $results = $crawler->filter('a')->each(function ($node, $i) use ($fp, $links) {
+
+
+            $links['url'] = $node->link()->getUri();
+            $links['author'] = 'Proton';
+
+            fwrite($fp, json_encode($links));
+            //return $links['url']; 
         });
         // Get text from a tag
-        $text = $crawler->filter('a')->each(function ($node,$i) use ($fp,$links) {
-             
-            $links['text']= $node->text();
-           fwrite($fp, json_encode($links)); 
+        $text = $crawler->filter('a')->each(function ($node, $i) use ($fp, $links) {
+
+            $links['text'] = $node->text();
+            fwrite($fp, json_encode($links));
         });
         // Get title from a tag    
-        $title = $crawler->filter('a[title]')->each(function ($node,$i) use ($fp,$links) {
-             
-            $links['title']= $node->text();
-             fwrite($fp, json_encode($links)); 
+        $title = $crawler->filter('a[title]')->each(function ($node, $i) use ($fp, $links) {
+
+            $links['title'] = $node->text();
+            fwrite($fp, json_encode($links));
+            return;
         });
-         fclose($fp);       
+        fclose($fp);
+
         return $url;
     }
 }
