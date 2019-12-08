73d0a55356f51836290a70d8821e443a8e2d0868
zmajnex <zmajnex@yahoo.com>
2019-12-08 13:36:02 +0100

    Changing controller logic

--- src/Controller/CrawlerController.php
+++ src/Controller/CrawlerController.php
@@ -8,11 +8,11 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\ParameterBag;
 
-use Goutte\Client;
+//use Goutte\Client;
 use Symfony\Component\DomCrawler\Crawler;
 use Symfony\Component\HttpClient\HttpClient;
-use Symfony\Component\DomCrawler\Crawler as DomCrawler;
-//use GuzzleHttp\Client; 
+
+use GuzzleHttp\Client; 
 class CrawlerController extends AbstractController
 {
  
@@ -31,39 +31,52 @@ class CrawlerController extends AbstractController
         
         $client = new Client();
         $links = array();
-        $crawler = $client->request('GET', $url);
-        $results = $crawler->filter('a');
-        $fp = fopen('results' . time() . '.json', 'w');
-        /**
-         * Anonymous Simfony function
-         * iterate trough $nodes, and write links in json 
-         * @return  array $links
-         */
-        //$links = new \stdClass();
+        $response = $client->request('GET', $url);
+        $statusCode = $response->getStatusCode();
+        $html = $response->getBody()->getContents();
+        $crawler = new Crawler($html);
+        $text = $crawler->filter('a')->eq(0)->text();
+        //$href = $crawler->filter('a')->attr('href')->eq(0);
+        //$title = $crawler->filter('a')->attr('title')->eq(0)->text();
+        var_dump($href);die;
+        foreach ($crawler as $domElement) {
+         var_dump($domElement->nodeName);
+     }
+     die;
+        //  $crawler = $client->request('GET', $url);
+
+      //   $results = $crawler->filter('a');
+      //   $fp = fopen('results' . time() . '.json', 'w');
+      //   /**
+      //    * Anonymous Simfony function
+      //    * iterate trough $nodes, and write links in json 
+      //    * @return  array $links
+      //    */
+      //   //$links = new \stdClass();
       
-        // Get href from a tag
+      //   // Get href from a tag
        
-        $results = $crawler->filter('a')->each(function ($node, $i) use ($fp, $links) {
-             $href = $node->link()->getUri();
-             $text = $node->text();       
-            $links['url'] = $href;
-            $links['author'] = 'Proton';
-            $links['text'] = $text;
+      //   $results = $crawler->filter('a')->each(function ($node, $i) use ($fp, $links) {
+      //        $href = $node->link()->getUri();
+      //        $text = $node->text();       
+      //       $links['url'] = $href;
+      //       $links['author'] = 'Proton';
+      //       $links['text'] = $text;
            
-           fwrite($fp, json_encode($links));                      
-        });
-           // Get title from a tag    
-           $title = $crawler->filter('a[title]')->each(function ($node, $i) use ($fp, $links) {
-           $title =  $node->text();
-            $links['title'] = $title; 
+      //      fwrite($fp, json_encode($links));                      
+      //   });
+      //      // Get title from a tag    
+      //      $title = $crawler->filter('a[title]')->each(function ($node, $i) use ($fp, $links) {
+      //      $title =  $node->text();
+      //       $links['title'] = $title; 
                      
-           fwrite($fp, json_encode($links));
+      //      fwrite($fp, json_encode($links));
         
-        });
+      //   });
        
-       // return $links;
+      //  // return $links;
       
-        fclose($fp);
+      //   fclose($fp);
        // return $url;
     }
 }
